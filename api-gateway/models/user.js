58ff9eccb9b97550d05e16547e9593397fe26900
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-05-20 19:32:24 -0400

    update

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -22,8 +22,14 @@ var userSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('User', userSchema);
 
-/**
-Database queries
-*/
+// user.save is used to add a new user in our database
+module.exports.add = (user, callback) => {
+    user.save(callback);
+};
+
+module.exports.getById = (id, callback) => {
+    var query = {id: id};
+    User.findById(query, callback);
+};
 
 //end program
